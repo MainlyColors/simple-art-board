d77f0afbeee4163d0eb15c3dc787c7db8d763bed
Ryan <ryanmahara@gmail.com>
2022-08-24 16:31:27 -0500

    add stories DB schema, way to render stories on dashboard page

---
+++ models/Story.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const StorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'public',
+    // list of possible values
+    enum: ['public', 'private'],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const StoryModel = mongoose.model('Story', StorySchema);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import { ensureAuth, ensureGuest } from '../middleware/auth.js';
+import { StoryModel } from '../models/Story.js';
 
 // for top level routes
 import express from 'express';
@@ -17,9 +18,21 @@ router.get('/', ensureGuest, (req, res) => {
 
 // protected route
 // check if auth already applied else put back on / route
-router.get('/dashboard', ensureAuth, (req, res) => {
-  console.log(req.user);
-  res.render('dashboard.hbs');
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    // lean() is a mongoose thing, to convert mongoose objects to plain JS objects
+    const stories = await StoryModel.find({ user: req.user.id }).lean();
+
+    res.render('dashboard.hbs', {
+      // user comes from passport
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log('💣💣💣 BANG BANG ERROR 💣💣💣');
+    console.error(err);
+    res.render('error/500');
+  }
 });
 
 export default router;
