0649b7697c7ed7946ca577099c3ebb6d25a1ee79
Ryan <ryanmahara@gmail.com>
2022-08-24 15:50:46 -0500

    add route protection by checking auth first before route happens

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+import { ensureAuth, ensureGuest } from '../middleware/auth.js';
+
 // for top level routes
 import express from 'express';
 const router = express.Router();
@@ -5,14 +7,18 @@ const router = express.Router();
 // @desc  Login/Landing page
 // @route GET /
 
-router.get('/', (req, res) => {
+// check if already authenticated then move to /dashboard else next
+router.get('/', ensureGuest, (req, res) => {
   res.render('login.hbs', { layout: 'login' });
 });
 
 // @desc  Dashboard
 // @route GET /dashboard
 
-router.get('/dashboard', (req, res) => {
+// protected route
+// check if auth already applied else put back on / route
+router.get('/dashboard', ensureAuth, (req, res) => {
+  console.log(req.user);
   res.render('dashboard.hbs');
 });
 
